48d7cfcbb2ba2886f8b117bb7bc20b44dbb9ef08
hellaxe <helaxe@gmail.com>
2014-05-13 01:30:38 +0300

    edited capistrano config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '95.85.28.126', user: 'deploy', roles: %w{web app}
 
 
 # Custom SSH Options
