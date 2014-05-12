582dfdcbe63ec418eb6db25eeb13827a16feed6a
hellaxe <helaxe@gmail.com>
2014-05-12 18:09:03 +0300

    added config.yml, implemented memory stats

--- app.rb
+++ app.rb
@@ -1,15 +1,30 @@
 require 'sinatra'
+require 'sinatra/config_file'
+require 'json'
+
+
 class VpsApiApplication < Sinatra::Base
-  
-  def '/'
+
+  register Sinatra::ConfigFile
+  config_file './config/config.yml'
+
+  get '/' do
 
   end
 
-  def '/stats'
-
+  get '/memory_stats' do
+    result = {}
+    File.open([settings.memory_cgroup_path, settings.memory_stat].join('/'), 'r') do |f|
+      while line = f.gets
+        key, val = line.split(' ')
+        result[key] = val
+      end
+    end
+    content_type :json
+    result.to_json
   end
 
-  def '/stats/:container_id'
+  get '/memory_stats/:container_id' do
 
   end
 
