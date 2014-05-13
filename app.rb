29d16c4d381d43d59c3aca9ce8d04833673cfca4
hellaxe <helaxe@gmail.com>
2014-05-13 18:04:17 +0300

    renamed memory url, implemented memstats for containers

--- app.rb
+++ app.rb
@@ -12,19 +12,44 @@ class VpsApiApplication < Sinatra::Base
 
   end
 
-  get '/memory_stats' do
-    result = {}
-    File.open([settings.memory_cgroup_path, settings.memory_stat].join('/'), 'r') do |f|
-      while line = f.gets
-        key, val = line.split(' ')
-        result[key] = val
-      end
-    end
+  get '/memory', provides: :json do
     content_type :json
-    result.to_json
+    begin
+      result = {}
+      File.open([settings.memory_cgroup_path, settings.memory_stat].join('/'), 'r') do |f|
+        while line = f.gets
+          key, val = line.split(' ')
+          result[key] = val
+        end
+      end
+      result.to_json
+    rescue Errno::ENOENT
+      status 404
+      {status: 404, reason: 'Not found'}.to_json
+    rescue
+      status 500
+      {status: 500, reason: 'Error'}.to_json
+    end
   end
 
-  get '/memory_stats/:container_id' do
+  get '/memory/:container_id', provides: :json do
+    content_type :json
+    begin
+      result = {}
+      File.open([settings.memory_cgroup_path, settings.docker_folder, params[:container_id], settings.memory_stat].join('/'), 'r') do |f|
+        while line = f.gets
+          key, val = line.split(' ')
+          result[key] = val
+        end
+      end
+        result.to_json
+    rescue Errno::ENOENT
+      status 404
+      {status: 404, reason: 'Not found'}.to_json
+    rescue
+      status 500
+      {status: 500, reason: 'Error'}.to_json
+    end
 
   end
 
